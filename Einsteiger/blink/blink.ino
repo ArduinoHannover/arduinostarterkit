e14bb1a2be532ea8524b2125880e13e703a1fe7c
Andreas Dunker <andreas-dunker@web.de>
2015-10-31 15:26:00 +0100

    uebersetzt

--- Einsteiger/blink/blink.ino
+++ Einsteiger/blink/blink.ino
@@ -1,17 +1,17 @@
 /*** Blink Sketch ***/
+
 const byte LED = 13;
 
 void setup ()
 {
-    // initialize digital pin 13 as an output.
+    // Pin 13 als Ausgabe
     pinMode (LED, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop ()
 {
-    digitalWrite (LED, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay (1000);              // wait for a second
+    digitalWrite (LED, HIGH);  // LED ein
+    delay (1000);              // eine Sekunde warten
     digitalWrite (LED, LOW);   // turn the LED off by making the voltage LOW
     delay (1000);              // wait for a second
 }
