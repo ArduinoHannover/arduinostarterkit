3f4bcfbf6f282b0c268cd68146e55b9e213c65a9
Luca Zimmermann <soundstorm@users.noreply.github.com>
2015-11-06 23:47:15 +0100

    Übersetzung erweitert, Detailinformationen

--- Einsteiger/blink/blink.ino
+++ Einsteiger/blink/blink.ino
@@ -1,17 +1,26 @@
 /*** Blink Sketch ***/
+/**
+ * Mit diesem Sketch wird die 
 
 const byte LED = 13;
+/*
+ * Alternativ LED_BUILTIN als Konstante nutzen, die fuer jedes Board (sofern definiert) gueltig ist.
+ * Sollte die LED bei dem von dir genutzen Board nicht an Pin 13 sein, so versuche es mit der Konstante.
+ * Wenn du eine LED, die nicht auf dem Board selbst angebracht ist schalten moechtest, so nutze folgendes:
+ * * Das kurze Bein (K, kurz, Kathode) der LED an GND (Ground/Masse/-)
+ * * Das lange Bein (LAng, Anode) ueber einen Widerstand an den Pin, der durch LED definiert wurde.
+*/
 
 void setup ()
 {
-    // Pin 13 als Ausgabe
+    // LED-Pin als Ausgang definieren, ansonsten ist ohne Definition der Pin als Eingang (INPUT)
     pinMode (LED, OUTPUT);
 }
 
 void loop ()
 {
-    digitalWrite (LED, HIGH);  // LED ein
-    delay (1000);              // eine Sekunde warten
-    digitalWrite (LED, LOW);   // turn the LED off by making the voltage LOW
+    digitalWrite (LED, HIGH);  // LED durch Schalten auf hohen Pegel (HIGH) einschalten
+    delay (1000);              // eine Sekunde (1000 Millisekunden) warten
+    digitalWrite (LED, LOW);   // LED durch Schalten auf niedrigen Pegel (LOW) ausschalten
     delay (1000);              // eine Sekunde warten
 }
