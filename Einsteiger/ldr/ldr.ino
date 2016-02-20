86b4e861274b69f9fc074a6694538adc21f27a77
Andreas Dunker <andreas-dunker@web.de>
2016-02-20 14:03:37 +0100

    ldr.ino hinzugefuegt

---
+++ Einsteiger/ldr/ldr.ino
@@ -0,0 +1,22 @@
+") == 0)
+towiderstand
+ *
+ *  siehe auch: http://wiki.arduino-hannover.de/index.php?title=Fotowiderstand
+ *
+ *  2015-11-05   Andreas Dunker    erzeugt
+ *
+ **********************************************************************/
+
+const byte INPUTPIN = 0; // analoger Pin 0 fuer den Eingang
+
+void setup ()
+{
+    Serial.begin (9600);
+}
+
+void loop ()
+{
+    int wert = analogRead (INPUTPIN);  // Wert lesen
+    Serial.println (wert);             // Wert ausgeben
+    delay (1000);                      // ein wenig warten
+}
